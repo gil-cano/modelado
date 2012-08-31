170218b624b9d0bd97f6d06d27b9931083a00d1e
Gildardo Bautista <gil.cano@gmail.com>
2012-08-31 13:23:48 -0500

    Ejemplo de Entrada y Salida

---
+++ clase03/standardinout.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    long p, q, r;
+    while (scanf("%ld %ld", &p, &q) != EOF){
+        if (q>p)
+            r = q-p;
+        else
+            r = p-q;
+        printf("%ld\n", r);
+    }
+    return 0;
+}
