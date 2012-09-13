364dae1eee76bf747a5a6974fd7a9e472806b08a
Gildardo Bautista <gil.cano@gmail.com>
2012-09-12 22:32:14 -0500

    test memset

---
+++ clase04/memset.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+
+    char registro[4] = {'000'};
+    // char c = '0';
+    // memset(registro, 0, sizeof(registro));
+    printf("%s\n", registro);
+    return 0;
+}
