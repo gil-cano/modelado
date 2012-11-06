e831d0baa1901d4a1571319fdf356a0b2183b32f
Gildardo Bautista <gil.cano@gmail.com>
2012-11-05 21:41:53 -0600

    finish example of events

--- clase09/RedRect.java
+++ clase09/RedRect.java
@@ -6,6 +6,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Dimension;
 import java.awt.event.*;
+import java.awt.Component;
+import java.awt.BorderLayout;
 
 public class RedRect extends Frame {
 
@@ -15,36 +17,32 @@ public class RedRect extends Frame {
 
     RedRect(){
         super("RedRect");
-        setSize(200,100);
-        // add("Center", new CVRedRect());
+        // setSize(200,100);
+        CVRedRect rec1 = new CVRedRect();
+        rec1.setSize(200, 100);
+        CVRedRect rec2 = new CVRedRect();
+        rec1.setSize(200, 100);
+        add(rec1, BorderLayout.NORTH);        
+        add(rec2, BorderLayout.CENTER);
+        pack();
         setVisible(true);
     }
 }
 
-
-// class CVRedRect extends Canvas {
+class CVRedRect extends Canvas {
     
-//     public void paint(Graphics g){
-//         Dimension d = getSize();
-//         int maxX = d.width -1;
-//         int maxY = d.height -1;
-//         g.drawString("d.width = " + d.width, 10, 30);
-//         g.drawString("d.height = " + d.height, 10, 60);
-//         g.setColor(Color.red);
-// g.drawLine(0,0, maxX,0);
-// g.drawLine(maxX,0, maxX,maxY);
-// g.drawLine(maxX,maxY, 0,maxY);
-// g.drawLine(0,maxY, 0,0);
-
-//         g.drawRect(0, 0, maxX, maxY);
-// g.fillRect(0,0,maxX.maxY);
-//     }
-// }
-
-// void board(Graphics g, int x, int y, int n, int w){
-//     for (int i=0; i<n; i++)
-//         for(int j=0; j<n; j++){
-//             g.setColor((i+n-j) % 2 == 0 ? Color.lightGray : Color.darkGRay);
-//             g.fillRect(x + i*w, y + j * w, w, w)
-//         }
-// }
+    public void paint(Graphics g){
+        Dimension d = getSize();
+        int maxX = d.width -1;
+        int maxY = d.height -1;
+        g.drawString("d.width = " + d.width, 10, 30);
+        g.drawString("d.height = " + d.height, 10, 60);
+        g.setColor(Color.red);
+        // g.drawLine(0,0, maxX,0);
+        // g.drawLine(maxX,0, maxX,maxY);
+        // g.drawLine(maxX,maxY, 0,maxY);
+        // g.drawLine(0,maxY, 0,0);
+        g.drawRect(0, 0, maxX, maxY);
+        // g.fillRect(0,0,maxX,maxY);
+    }
+}
